789f781942306e9e0424323c734a458d6bc01e5d
A-Jatav <108002072+A-Jatav@users.noreply.github.com>
2023-04-03 12:50:32 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,8 +4,10 @@ var leftWristX = 0;
 var leftWristY = 0;
 var rightWristX = 0;
 var rightWristY = 0;
-var score = 0;
-song1_playing = "";
+var score1 = 0;
+var song1_playing = "";
+var score2 = 0;
+var song2_playing = "";
 
 function preload() {
     song1 = loadSound("imperial-march.mp3");
@@ -28,8 +30,9 @@ function gotPoses(results) {
         leftWristY = results[0].pose.leftWrist.y;
         rightWristX = results[0].pose.rightWrist.x;
         rightWristY = results[0].pose.rightWrist.y;
-        score = results[0].pose.keypoints[9].score;
-        console.log("Left wrist x = " + leftWristX + ", left wrist y = " + leftWristY + ", right wrist x = " + rightWristX + ", right wrist y = " + rightWristY);
+        score1 = results[0].pose.keypoints[9].score;
+        score2 = results[0].pose.keypoints[10].score;
+        console.log("Score of left wrist = " + score1 + ", score of right wrist = " + score2 + "left wrist x = " + leftWristX + ", left wrist y = " + leftWristY + ", right wrist x = " + rightWristX + ", right wrist y = " + rightWristY);
     };
 };
 
@@ -42,12 +45,21 @@ function draw() {
     fill("red");
     stroke("red");
     song1_playing = song1.isPlaying();
-    if(score > 0.05){
+    if (score1 > 0.05) {
         circle(leftWristX, leftWristY, 20);
         song2.stop();
-        if(song1_playing == false){
+        if (song1_playing == false) {
             song1.play();
             document.getElementById("song_name").innerHTML = "Song Playing: Imperial March - Darth Vader's Theme";
+        };
+    };
+    song2_playing = song2.isPlaying();
+    if(score2 > 0.05){
+        circle(rightWristX, rightWristY, 20);
+        song1.stop();
+        if(song2_playing == false){
+            song2.play();
+            document.getElementById("song_name").innerHTML = "Song Playing: The Force Theme - Luke Skywalker's Theme";
         };
     };
 };
